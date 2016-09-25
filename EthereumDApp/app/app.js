59c8530504af3a660ccb9be87b6100ca480fc091
Thomas Conte <thomas@conte.com>
2016-09-25 20:24:53 +0200

    Use Bletchley addresses

--- EthereumDApp/app/app.js
+++ EthereumDApp/app/app.js
@@ -54,11 +54,11 @@ $('.pdf-link').click(function() {
   });
 });
 
-var contract;
+var contract, logs;
 
 $(document).ready(function() {
-  contract = ChainPoint.deployed();
-  var logs = contract.CheckPointAchieved({fromBlock: "latest"});
+  contract = ChainPoint.at('0x4611c52fe74bbeb6274b9e9328c9d31e3b7ab54b');
+  logs = contract.CheckPointAchieved({fromBlock: 'latest'});
   logs.watch(function(error, result) {
     console.log("CheckPoint!");
     console.log(result.args.userid);
@@ -72,7 +72,9 @@ $(document).ready(function() {
 function sendToBlockchain(id, user, step) {
   var account_testrpc = "0x27cf95ea11d12ae58f4ab9e1dbe568eae9294ccf";
   var account_production = "0xa4cc9db2ac66daf6b3a99f7064fa6d3e598cb7e8";
-  contract.check(id, user.Firstname, step, {from: account_testrpc, gas:1000000}).then(function(tx) {
+  var account_devthomas = "0x87b3f6def4d451c41be733b8924da66dea0caed4";
+  var account_bletchley = "0x708C77773a1c379aA70B0402Fa0dF12A9B00D76A";
+  contract.check(id, user.Firstname, step, {from: account_bletchley}).then(function(tx) {
     // Transaction successful
     console.log("Transaction successful! " + tx);
   }).catch(function(e) {
