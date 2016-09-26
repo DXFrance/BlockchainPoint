3ea00aa304a89b4974427d75bcd4861d717d26a9
Sofiene Djebali <t-sodje@microsoft.com>
2016-09-26 16:55:43 +0200

    Checkpoints & Certifications

--- EthereumDApi/app.js
+++ EthereumDApi/app.js
@@ -1,15 +1,37 @@
 var express     = require('express');
 var app         = express();
-
+var fs = require('fs');
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-
+var certified = JSON.parse(fs.readFileSync('certified.json'));
 var bodyParser  = require('body-parser');
-var fs = require('fs');
 var pdf = require('html-pdf');
 var ejs = require('ejs');
 var html = fs.readFileSync('pdf.html', 'utf-8');
-var registered = JSON.parse(fs.readFileSync('registered.json'));
+var config = require('./config');
+var token;
+var request = require('sync-request');
+var user_complete = certified;
+var oauth2 = require('simple-oauth2').create({
+  client: {
+    id: config.client_id,
+    secret: config.client_secret
+  },
+  auth: {
+    tokenHost: config.host,
+    tokenPath: config.token_url,
+    authorizePath: config.auth_url
+  }
+});
+
+oauth2.clientCredentials.getToken({scope: config.scope}, (error, result) => {
+  if (error) {
+    return console.log('Access Token Error', error.message);
+  }
+  token = oauth2.accessToken.create(result).token.access_token;
+  console.log('Token : ' + token);
+  setUpBlockChainWatch();
+});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -30,32 +52,8 @@ app.use(bodyParser.urlencoded({
 
 app.post('/auth', function(req, res){
   var id = req.body.id;
-  for (var i = 0, len = registered.length; i < len; i++) {
-    var ids = registered[i].Badge.split('/');
-    console.log(id, ids[ids.length - 1]);
-    if (ids[ids.length - 1] == id) {
-      console.log('MATCH');
-      return res.json(registered[i]);
-    }
-  }
 
-  return res.json({error: true});
-});
-
-app.post('/pdf', function(req, res) {
-  var user = req.body.user;
-  var ids = user.Badge.split('/');
-  var id = ids[ids.length - 1];
-
-  var exist = statPath('public/' + id + '.pdf');
-  if(exist && exist.isFile()) {
-    var html_data = ejs.render(html, { firstname: user.Firstname, lastname: user.Lastname });
-    pdf.create(html_data, {format: 'Letter'}).toFile('public/' + id + '.pdf', function(err, response) {
-      res.send('http://localhost:1996/' + id + '.pdf');
-    });
-  } else {
-    res.send('http://localhost:1996/' + id + '.pdf');
-  }
+  return res.json(getUser(id));
 });
 
 // Blockchain events
@@ -63,19 +61,12 @@ app.post('/pdf', function(req, res) {
 var ChainPoint = require('./ChainPoint.sol.js');
 var Web3 = require('web3');
 
-var abi = ChainPoint.all_networks[112358].abi;
-var address = ChainPoint.all_networks[112358].address;
+var abi = ChainPoint.all_networks['default'].abi;
+var address = "0x39f0a2ec78069eb5f37934d59c85c8c584778157";
 
-var web3 = new Web3(new Web3.providers.HttpProvider("http://tconte4kv.northeurope.cloudapp.azure.com:8545"));
+var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 var contract = web3.eth.contract(abi).at(address);
-
-var logs = contract.JourneyAchieved({fromBlock: 'latest'});
-logs.watch(function(error, result) {
-  console.log("Journey Achieved!");
-  console.log(result.args.userid);
-  console.log(result.args.username);
-});
 
 // Run server
 
@@ -89,5 +80,59 @@ function statPath(path) {
 }
 
 io.on('connection', function (socket) {
-  console.log('new client');
+  socket.emit('user_complete', user_complete);
+  socket.on('checkpoint_begin', function(data) {
+    socket.broadcast.emit('checkpoint_begin', data);
+    console.log('checkpoint_begin');
+  });
+  socket.on('checkpoint_mined', function(data) {
+    socket.broadcast.emit('checkpoint_mined', data);
+    console.log('checkpoint_mined');
+  });
 });
+
+function getUser(id) {
+  var user = request('GET', 'https://api.inwink.com/' + config.event_id + '/registered/' + id, {
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + token
+      }
+  });
+  user = JSON.parse(user.getBody('utf8'));
+
+  return user;
+}
+
+function createLink(link) {
+  var bitly = request('GET', 'https://api-ssl.bitly.com/v3/user/link_save?access_token=8e21360de7ecca4648c3f588b5919c15ea7dde63&longUrl='+link);
+
+  bitly = JSON.parse(bitly.getBody('utf8')).data.link_save.link;
+  return bitly;
+}
+
+function setUpBlockChainWatch() {
+  var logs = contract.JourneyAchieved({fromBlock: 'latest'});
+
+  logs.watch(function(error, result) {
+    if (typeof result === 'undefined') {
+      return;
+    }
+    for (var i = 0, len = user_complete.length; i < len; i++) {
+      if (user_complete[i].id == result.args.userid) {
+        //User already have a certification
+        return;
+      }
+    }
+    console.log("Journey Achieved!");
+    console.log(result.args.userid);
+    console.log(result.args.username);
+    var user = getUser(result.args.userid);
+    var html_data = ejs.render(html, { firstname: user.firstname, lastname: user.lastname });
+    pdf.create(html_data, {format: 'Letter'}).toFile('public/' + result.args.userid + '.pdf', function(err, response) {
+      var user_complete_new = {id: result.args.userid, username: result.args.username, pdf: createLink('http://127.0.0.1:1996/' + result.args.userid + '.pdf')};
+      user_complete.push(user_complete_new);
+      fs.writeFile('certified.json', JSON.stringify(user_complete), 'utf8');
+      io.sockets.emit('user_complete_new', user_complete_new);
+    });
+  });
+}
--- EthereumDApp/app/app.js
+++ EthereumDApp/app/app.js
@@ -1,3 +1,11 @@
+if (localStorage.getItem("machine_id") === null) {
+  var machine_id = prompt('ID de la machine');
+  localStorage.setItem('machine_id', machine_id);
+} else {
+  var machine_id = localStorage.getItem("machine_id");
+}
+
+
 var http = false;
 var user = null;
 $('#reader').html5_qrcode(function(data){
@@ -29,53 +37,48 @@ $('#reader').html5_qrcode(function(data){
     }
     console.log(data);
     user = data;
-    $('.pdf').fadeIn();
     $('.loader').fadeOut(function() {
       $('.anonymous').fadeOut();
-      $('.well').css('display', 'block').addClass('animated flipInX').text('Welcome ' + user.Firstname + ' ' + user.Lastname + ' | ' + user.Mail + ' from ' + user.Pays);
+      $('.well').css('display', 'block').addClass('animated flipInX').text('Welcome ' + user.firstname + ' ' + user.lastname + ' | ' + user.mail + ' from ' + user.country.value);
     });
 
     // Send to blockchain
-    sendToBlockchain(id, user, 1);
+    sendToBlockchain(id, user, machine_id);
 
     // Release lock
     http = false;
   });
 }, function() {
-  // No QR code detected 
+  // No QR code detected
 }, function() {
   // Error
-});
-
-$('.pdf-link').click(function() {
-  $.post('http://localhost:1996/pdf', {user: user}, function(data) {
-    console.log(data);
-    alert(data);
-  });
 });
 
 var contract, logs;
 
 $(document).ready(function() {
-  contract = ChainPoint.at('0x4611c52fe74bbeb6274b9e9328c9d31e3b7ab54b');
+  contract = ChainPoint.at('0x39f0a2ec78069eb5f37934d59c85c8c584778157');
   logs = contract.CheckPointAchieved({fromBlock: 'latest'});
   logs.watch(function(error, result) {
     console.log("CheckPoint!");
     console.log(result.args.userid);
     console.log(result.args.username);
     console.log(result.args.step.toString());
+    DOM_pushCheckpointDone(result.args.username, result.args.step.toString());
+    socket.emit('checkpoint_mined', {username: result.args.username, step: result.args.step.toString()});
   });
 });
 
 // Send transaction to blockchain
 
 function sendToBlockchain(id, user, step) {
-  var account_testrpc = "0x27cf95ea11d12ae58f4ab9e1dbe568eae9294ccf";
+  var account_testrpc = "0x9de967378ed802b954e3be289cc5d598021c7ffe";
   var account_production = "0xa4cc9db2ac66daf6b3a99f7064fa6d3e598cb7e8";
   var account_devthomas = "0x87b3f6def4d451c41be733b8924da66dea0caed4";
   var account_bletchley = "0x708C77773a1c379aA70B0402Fa0dF12A9B00D76A";
-  contract.check(id, user.Firstname, step, {from: account_bletchley}).then(function(tx) {
-    // Transaction successful
+  contract.check(id, user.firstname, step, {from: account_testrpc}).then(function(tx) {
+    DOM_pushCheckpoint(user.firstname, step);
+    socket.emit('checkpoint_begin', {username: user.firstname, step: step});
     console.log("Transaction successful! " + tx);
   }).catch(function(e) {
     // Transaction failed
@@ -83,3 +86,34 @@ function sendToBlockchain(id, user, step) {
     console.dir(e);
   });
 }
+
+function DOM_pushCheckpoint(firstname, step) {
+  $('.table-checkpoints').find('tbody').append('<tr><td>#' + firstname + ' - <i class="fa fa-clock-o"></i> Checkpoint #' + step + ' - En cours de minage ...</td></tr>');
+}
+
+function DOM_pushCheckpointDone(firstname, step) {
+  $('.table-checkpoints').find('tbody').append('<tr><td>#' + firstname + ' - <i class="fa fa-check"></i> Checkpoint #' + step + ' - Miné !</td></tr>');
+}
+
+function DOM_pushCertification(firstname, pdf) {
+  $('.table-certifs').find('tbody').append('<tr><td>' + firstname + ' - <i class="fa fa-user"></i> Certifiée - <a href="'+ pdf +'" target="_blank" title="Certification">'+ pdf +'</a></td></tr>');
+}
+
+var socket = io.connect('http://localhost:1996');
+socket.on('user_complete', function(users_complete) {
+  for (var i = 0, len = users_complete.length; i < len; i++) {
+    DOM_pushCertification(users_complete[i].username, users_complete[i].pdf);
+  }
+});
+
+socket.on('user_complete_new', function(user_complete) {
+  DOM_pushCertification(user_complete.username, user_complete.pdf);
+});
+
+socket.on('checkpoint_begin', function(user_checkpoint) {
+  DOM_pushCheckpoint(user_checkpoint.username, user_checkpoint.step);
+});
+
+socket.on('checkpoint_mined', function(user_checkpoint) {
+  DOM_pushCheckpointDone(user_checkpoint.username, user_checkpoint.step);
+});
